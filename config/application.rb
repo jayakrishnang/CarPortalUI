02f829e051f8312f0965a1f2e2c1b1567c0b77be
Pravda Jithray <pravda.mec@gmail.com>
2015-09-08 15:57:43 +0530

    Modified code

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-config.assets.initialize_on_precompile = false
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module CarPortal
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
