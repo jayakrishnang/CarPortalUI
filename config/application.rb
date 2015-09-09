6ab4c0f75f63b5d114f8686617b44c33dd705d6b
Arun Mathew Kurian <amkurian@gmail.com>
2015-09-09 12:51:45 +0530

    assest precompile configuration added to application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module CarPortal
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
